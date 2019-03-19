030417bd6ba1a111e78a090e07c62db99a228687
avalynn <48425648+avalynn@users.noreply.github.com>
2019-03-20 00:34:43 +0800

    Update and rename Client.C to Client.c

--- Client.c
+++ Client.c
@@ -6,7 +6,7 @@ include <sys/types.h>
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
-#include (unistd.h>
+#include <unistd.h>
 define PORT 43454
 define MAX 80
 define SA struct sockaddr
@@ -17,7 +17,7 @@ char buff[MAX];
 int n;
 for(;;)
 {
-bzero(buff,sizeof((buff));
+bzero(buff,sizeof(buff));
 printf("Enter the string : ");
 n=0;
 while((buff[n++]=getchar())!='\n');
@@ -42,15 +42,15 @@ sockfd=socket(AF_INET,SOCK_STREAM,0);
 if(sockfd ==-1)
 {
 printf("Socket creation failed ... \n");
-exit(0)
+exit(0);
 }
 
 else
 printf("Socket successfully created .... \n");
-bzero(&serveraddr, sizeof(serveraddr));
-serveraddr.sin_family = AF_INET;
-serveraddr.sin_addr.s_addr = inet_addr("192.18.40.128");
-serveraddr.sin_port = htons(PORT);
+bzero(&servaddr, sizeof(servaddr));
+servaddr.sin_family = AF_INET;
+servaddr.sin_addr.s_addr = inet_addr("192.168.146.131");
+servaddr.sin_port = htons(PORT);
 
 if(connect(sockfd, (SA *)&servaddr,sizeof(servaddr))!=0)
 {
@@ -59,7 +59,7 @@ exit(0);
 }
 
 else
-printf("Connected to server..\n);
+printf("Connected to server..\n");
 func(sockfd);
 close(sockfd);
 
