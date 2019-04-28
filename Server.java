40fed2f420b5076eb7bbf8634b1d70dc0219e94b
avalynn <48425648+avalynn@users.noreply.github.com>
2019-04-28 23:03:42 +0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,33 +1,60 @@
-import java.io.*;
 import java.net.*;
-class Server
-{
-public static void main(String[] args)throws Exception
-{
-ServerSocket serversocket = new ServerSocket(8080);
-System.out.println("Server dah sedia ni!");
-Socket sock = serversocket.accept();
-System.out.println("Waalaikummussalam");
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+class SimpleServer{
 
-BufferedReader keyRead = new BufferedReader(new 
-InputStreamReader(System.in));
-OutputStream ostream = sock.getOutputStream();
-PrintWriter pwrite = new PrintWriter(ostream,true);
+    public static void main (String[] args)
+    {
+      try{      
+      //Defining/opening connection
+      ServerSocket srvr = new ServerSocket(5140);
+      Socket skt = srvr.accept();
+      
+      InputStreamReader bf = new InputStreamReader(skt.getInputStream());
+      BufferedReader in= new BufferedReader(bf);
+      PrintWriter out = new PrintWriter(skt.getOutputStream(),true);
+      Scanner s=new Scanner(System.in);
+      
+      Thread send=new Thread(new Runnable(){
+      String msg;
+      @Override
+      public void run(){
+            while(true){
+  
+                  msg=s.nextLine();
+                  out.print(msg);
+                  out.flush();
+            }
+      }
+      });
+      send.start();
+      
+      Thread receive=new Thread(new Runnable(){
+      String msg;
+      @Override
+      public void run(){
+            while(true){
+                  try{
+                        msg=in.readLine();
+                  }catch(IOException e){
+                        e.printStackTrace();
+                  }
+                  System.out.print("From Client: "+msg);
+            }
+      }
+      });
+      receive.start();
+            
+      
+      
+      //closing connection
+      //out.close();
+      //skt.close();
+      //srvr.close();
+      }catch (IOException e){
+            e.printStackTrace();
+      }
+    }
+}
 
-InputStream istream = sock.getInputStream();
-BufferedReader receiveRead = new BufferedReader(new 
-InputStreamReader(istream));
-
-String receiveMessage,sendMessage;
-while(true)
-{
-if((receiveMessage=receiveRead.readLine())!=null)
-{
-System.out.println(receiveMessage);
-}
-sendMessage = keyRead.readLine();
-pwrite.println(sendMessage);
-pwrite.flush();
-}
-}
-}
